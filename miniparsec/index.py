ad2087142758243a02b9ed66347d4185e531e1b0
Maximilien de Dinechin <m.dinechin@protonmail.com>
2023-06-06 19:09:35 +0200

    unidecode

--- miniparsec/index.py
+++ miniparsec/index.py
@@ -1,13 +1,15 @@
 import re
 from pathlib import Path
 
+from unidecode import unidecode
+
 
 def file_index(path: Path, min_length: int = 2) -> tuple[set[str], int]:
     word_count = 0
     index = set()
     with open(path, "r", encoding="utf-8") as f:
         for line in f:
-            words_raw = re.findall(r"\w+", line.lower())
+            words_raw = re.findall(r"[a-z]+", unidecode(line.lower()))
             words = set(str(w) for w in words_raw if len(w) >= min_length)
             index.update(words)
             word_count += len(words)
